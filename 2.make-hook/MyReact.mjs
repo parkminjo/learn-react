dccaea1f05318532ae2532093915fee23fccd4fa
parkminjo <cruiser367@naver.com>
2025-07-03 00:25:08 +0900

    feat: 한계를 보완한 useState 훅 구현

--- 2.make-hook/MyReact.mjs
+++ 2.make-hook/MyReact.mjs
@@ -1,9 +1,29 @@
+// 여러 개의 value를 갖기 위해 value를 배열로 관리할 필요가 있음
+let hooks = [],
+  currentHook = 0;
+
 const MyReact = {
   render(Component) {
     const Comp = Component(); // Component가 호출되어 객체를 반환함
     Comp.render(); // 객체의 메서드를 호출함
+    currentHook = 0; // 배열의 인덱스 초기화
     return Comp; // 객체를 반환함
   },
+};
+
+export const useState = (initialValue) => {
+  hooks[currentHook] = hooks[currentHook] || initialValue;
+  const hookIndex = currentHook;
+
+  const setState = (newState) => {
+    if (typeof newState === 'function') {
+      hooks[hookIndex] = newState(hooks[hookIndex]);
+    } else {
+      hooks[hookIndex] = newState;
+    }
+  };
+
+  return [hooks[currentHook++], setState];
 };
 
 export default MyReact;
--- 2.make-hook/index.mjs
+++ 2.make-hook/index.mjs
@@ -1,11 +1,20 @@
-import MyReact from './MyReact.mjs';
+import MyReact, { useState } from './MyReact.mjs';
 
 function FunctionalComponent() {
+  const [count, setCount] = useState(0);
+  const [text, setText] = useState('foo');
+
   return {
+    click: () => setCount((prev) => prev + 1),
+    text: (text) => setText(text),
     render: () => {
-      console.log('render');
+      console.log('render', { count, text });
     },
   };
 }
 
 let App = MyReact.render(FunctionalComponent);
+App.click();
+App = MyReact.render(FunctionalComponent);
+App.text('bar');
+App = MyReact.render(FunctionalComponent);
